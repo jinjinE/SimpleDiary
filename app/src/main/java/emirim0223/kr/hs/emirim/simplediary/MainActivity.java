6b953eacb00803901b01692e87296a1c43fe659a
every-sushi <nadia0515@naver.com>
2017-05-30 20:47:36 +0900

    #3 Current datd and DatePicker Init and Handle

--- app/src/main/java/emirim0223/kr/hs/emirim/simplediary/MainActivity.java
+++ app/src/main/java/emirim0223/kr/hs/emirim/simplediary/MainActivity.java
@@ -1,16 +1,19 @@
 package emirim0223.kr.hs.emirim.simplediary;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
     DatePicker date;
     EditText edit;
     Button but;
-
+    String fileName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +21,23 @@ public class MainActivity extends AppCompatActivity {
         date = (DatePicker)findViewById(R.id.date_pick);
         edit = (EditText)findViewById(R.id.edit);
         but = (Button) findViewById(R.id.but);
+
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DATE);
+
+        date.init(year, month, day, new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker datePicker, int year, int month, int day) {
+                fileName = year + "_" + (month+1) + "_"+day+".txt";
+                String readData = readDiary(fileName);
+                edit.setText(readData);
+                but.setEnabled(true);
+            }
+        });
+    }
+    public String readDiary(String fileName){
+        return null;
     }
 }
